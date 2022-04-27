ce5a5efb9b487117d546b7a12e47789e79e184c4
pandorasbread <pandorasbread@gmail.com>
2022-04-26 18:58:11 -0600

    lets gooooo

---
+++ FlappyBorb/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+
+    private void Start()
+    {
+        gameOverCanvas.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    public void gameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
---
+++ FlappyBorb/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D body;
+    private const float playerSpeed = .5f;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (body.velocity.y > 6)
+            body.velocity = new Vector2(body.velocity.x, 6);
+        if (body.velocity.y < -6)
+            body.velocity = new Vector2(body.velocity.x, -6);
+
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            animator.Play("flap");
+            body.velocity += new Vector2(0, 3);
+        }
+
+        if (Input.GetKey("a"))
+            body.position += Vector2.left * Time.fixedDeltaTime * playerSpeed;
+
+        if (Input.GetKey("d"))
+            body.position += Vector2.right * Time.fixedDeltaTime * playerSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "death")
+            gameManager.gameOver();
+    }
+}
